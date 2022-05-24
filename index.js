dc6541cb4291b2409066c8dbd5c3e32b7acdecaa
Marcos Del Sol Vives <marcos@orca.pet>
2022-05-24 16:30:00 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const readline = require('node:readline');
+const { program } = require('commander');
+const { spawn } = require('node:child_process');
+
+program
+	.name('git-update-authors')
+	.description("Generate AUTHORS file based on Git's history")
+	.option('-o, --output <file>', 'write result to this file', 'AUTHORS');
+program.parse();
+const options = program.opts();
